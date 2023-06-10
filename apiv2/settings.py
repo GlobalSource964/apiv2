db195de2ef168389b698792a549ca64d9849cbf7
dogan <dogancihatkayanews@gmail.com>
2023-06-10 13:39:32 +0300

    v2.0.8

--- apiv2/settings.py
+++ apiv2/settings.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
 
 CORS_ALLOWED_ORIGINS = [
     "https://apiv2.ayasescorts.online",
+    "http://apiv2.ayasescorts.online",
     # Diğer güvendiğiniz siteleri buraya ekleyebilirsiniz.
 ]
 
