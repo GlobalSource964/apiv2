af2e6514835550c11789f39fc81e243624ecf676
dogan <dogancihatkayanews@gmail.com>
2023-06-10 16:13:31 +0300

    v2.0.13

--- apiv2/urls.py
+++ apiv2/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v2/', include('ilans.urls'))
 ]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
