03ba3b574c118378c225fbe2353dde277bc70623
dogan <dogancihatkayanews@gmail.com>
2023-06-10 16:29:39 +0300

    v2.0.18

--- apiv2/urls.py
+++ apiv2/urls.py
@@ -5,7 +5,7 @@ from django.urls import path, include
 from apiv2 import settings
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('dadybro/', admin.site.urls),
     path('api/v2/', include('ilans.urls'))
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
