de0ccb5c96b057ebebe56114009796077db24c6a
Salih Can <salihcan9946@gmail.com>
2023-06-17 16:06:53 +0300

    v2.0.56

--- ilans/urls.py
+++ ilans/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 from .views import IlanList, IlanDetail, DomainList, DomainDetail, ResimList, ResimDetail, TransactionList, \
-    TransactionDetail, BlogListCreate, BlogRetrieveUpdateDestroy
+    TransactionDetail, BlogListCreate, BlogRetrieveUpdateDestroy, DomainListbacklink
 
 urlpatterns = [
     path('ilanlar', IlanList.as_view(), name='ilan-list'),
     path('ilanlar/<int:pk>', IlanDetail.as_view(), name='ilan-detail'),
     path('domain', DomainList.as_view(), name='domain-list'),
-    path('domainbacklink', DomainList.as_view(), name='domain-list'),
+    path('domainbacklink', DomainListbacklink.as_view(), name='domain-list'),
     path('domain/<int:pk>', DomainDetail.as_view(), name='domain-detail'),
     path('resim', ResimList.as_view(), name='resim-list'),
     path('resim/<int:pk>', ResimDetail.as_view(), name='resim-detail'),
