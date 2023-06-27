36e882630181dd6da66043a1acb91035cd15a67d
Salih Can <salihcan9946@gmail.com>
2023-06-27 04:43:32 +0300

    v2.0.65

--- ilans/views.py
+++ ilans/views.py
@@ -1,12 +1,14 @@
 from rest_framework import generics
-
 from apiv2 import settings
 from .models import Ilan, Domain, Resim, Transaction, Blog
 from .permissions import ApiKeyPermission
+from django.views.decorators.cache import cache_page
+from django.utils.decorators import method_decorator
 from .serializers import IlanSerializer, DomainSerializer, ResimSerializer, TransactionSerializer, BlogSerializer, \
     DomainSerializerBacklink
 
 
+@method_decorator(cache_page(60*60*2), name='get')  # Cache for 2 hours
 class IlanList(generics.ListCreateAPIView):
     serializer_class = IlanSerializer
     permission_classes = [ApiKeyPermission]
@@ -24,55 +26,69 @@ class IlanList(generics.ListCreateAPIView):
             else:
                 return Ilan.objects.none()  # Eğer bu domain yoksa, hiç ilan döndürme
 
+
+@method_decorator(cache_page(60*60*2), name='get')  # Cache for 2 hours
 class IlanDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Ilan.objects.all()
     serializer_class = IlanSerializer
     permission_classes = [ApiKeyPermission]
 
 
+@method_decorator(cache_page(60*60*2), name='get')  # Cache for 2 hours
 class DomainList(generics.ListCreateAPIView):
     queryset = Domain.objects.all()
     serializer_class = DomainSerializer
     permission_classes = [ApiKeyPermission]
 
+
+@method_decorator(cache_page(60*60*2), name='get')  # Cache for 2 hours
 class DomainListbacklink(generics.ListCreateAPIView):
     queryset = Domain.objects.all()
     serializer_class = DomainSerializerBacklink
     permission_classes = [ApiKeyPermission]
 
 
+@method_decorator(cache_page(60*60*2), name='get')  # Cache for 2 hours
 class DomainDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Domain.objects.all()
     serializer_class = DomainSerializer
     permission_classes = [ApiKeyPermission]
 
 
+@method_decorator(cache_page(60*60*2), name='get')  # Cache for 2 hours
 class BlogListCreate(generics.ListCreateAPIView):
     queryset = Blog.objects.all()
     serializer_class = BlogSerializer
 
+
+@method_decorator(cache_page(60*60*2), name='get')  # Cache for 2 hours
 class BlogRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
     queryset = Blog.objects.all()
     serializer_class = BlogSerializer
 
+
+@method_decorator(cache_page(60*60*2), name='get')  # Cache for 2 hours
 class ResimList(generics.ListCreateAPIView):
     queryset = Resim.objects.all()
     serializer_class = ResimSerializer
     permission_classes = [ApiKeyPermission]
 
 
+@method_decorator(cache_page(60*60*2), name='get')  # Cache for 2 hours
 class ResimDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Resim.objects.all()
     serializer_class = ResimSerializer
     permission_classes = [ApiKeyPermission]
 
 
+@method_decorator(cache_page(60*60*2), name='get')  # Cache for 2 hours
 class TransactionList(generics.ListCreateAPIView):
     queryset = Transaction.objects.all()
     serializer_class = TransactionSerializer
     permission_classes = [ApiKeyPermission]
 
 
+@method_decorator(cache_page(60*60*2), name='get')  # Cache for 2 hours
 class TransactionDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Transaction.objects.all()
     serializer_class = TransactionSerializer
